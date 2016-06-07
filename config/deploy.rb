bc675fb1f622d4812dfedceac3819f4917dd82a6
Raúl Acuña <reacuna@crac.cl>
2016-06-06 21:23:08 -0400

    Change capistrano log level to info

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :repo_url, 'git@github.com:reacuna/reacuna.me.git'
 # set :format, :pretty
 
 # Default value for :log_level is :debug
-# set :log_level, :debug
+set :log_level, :info
 
 # Default value for :pty is false
 # set :pty, true
